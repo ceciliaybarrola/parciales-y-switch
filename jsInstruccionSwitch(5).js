0d227df23bf1b59a8e05cc4299f5d6af3bb641cd
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-13 09:31:10 -0300

    Update jsInstruccionSwitch(5).js

--- jsInstruccionSwitch(5).js
+++ jsInstruccionSwitch(5).js
@@ -5,7 +5,17 @@ var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
 	
-	
+	switch (laHora)
+{
+    case "7" :
+    case "8" :
+    case "9" :
+    case "10" :
+    case "11" :
+        alert("es de mañana");
+        break;
+
+}
 
 
 
