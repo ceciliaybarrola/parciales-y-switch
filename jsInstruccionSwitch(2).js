171f4b2291e87e9cdf56d528f1b4289d13910854
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-13 08:39:12 -0300

    Update jsInstruccionSwitch(2).js

--- jsInstruccionSwitch(2).js
+++ jsInstruccionSwitch(2).js
@@ -1,10 +1,35 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
 
-alert (mesDelAño);
+alert (mesDelAnio);
 
+switch (mesDelAnio)
+{   
+   
+    case "Julio" :
+    case "Agosto" :
+        alert("Abrigate que hace frio.");
+        break;
+    case "Septiempre" :
+    case "Octubre" :
+    case "Noviembre" :
+    case "Diciembre" :
+        alert("Ya pasamos el frio, ahora calor!!!.")
+        break;
+    case "Enero" :
+    case "Febrero" :
+    case "Marzo" :
+    case "Abril" :
+    case "Mayo" :
+    case "Junio" :
+             alert("Falta para el invierno.")
+             break;
+             // puedo usar default: alert
+             // hay que tener cuidado cuando el usuario escribe
+    
+}
 
 
 
