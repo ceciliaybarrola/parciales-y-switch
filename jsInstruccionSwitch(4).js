b579386bd5237db7fbcff638bdf1a990205297d4
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-13 09:30:30 -0300

    Update jsInstruccionSwitch(4).js

--- jsInstruccionSwitch(4).js
+++ jsInstruccionSwitch(4).js
@@ -1,11 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch (mesDelAnio)
+{
+    case ("Febrero") :
+        alert("Este mes tiene 28 días");
+        break;
+    case ("Abril") :
+    case ("Junio") :
+    case ("Agosto") :
+    case ("Septiempre") :
+        alert("Este mes tiene 30 días");
+        break;
+    default :
+        alert("Este mes tiene 31 días");
+        break;
+        
+}
+		
 
 
 
