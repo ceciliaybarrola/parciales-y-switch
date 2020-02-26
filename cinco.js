73b5853538e3159398ae517366ef42c5230a7818
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-26 20:05:43 -0300

    Update cinco.js

--- cinco.js
+++ cinco.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
+var planeta = prompt("Ingrese un planeta");
+
+switch (planeta)
+{
+    case ("mercurio"):
+    case ("venus"):
+        alert("Acá hace mas calor");
+        break;
+    case ("tierra"):
+        alert("Aca vivimos");
+        break;
+    case ("marte"):
+    case ("jupiter"):
+    case ("saturno"):
+    case ("urano"):
+    case ("neptuno"):
+    case ("pluton"):
+        alert("Acá hace mas frio");
+        break;
+    default:
+        alert("No es un planeta");
+}
 
 }
