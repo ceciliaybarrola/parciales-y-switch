494660cb46f6e3c782599d0ee08d10840100165c
ceciliaybarrola <60697915+ceciliaybarrola@users.noreply.github.com>
2020-02-26 20:08:05 -0300

    Update siete.js

--- siete.js
+++ siete.js
@@ -1,4 +1,37 @@
 function mostrar()
 {
+ var num;
+    var i=0;
+    var sexo;
+    var contFem=0;
+    var contMasc=0;
+    var promedio;
+    var min=num;
+    var nota=0;
+
+    num=parseInt(num);
+
+    while (i<5)
+    {
+       // sexo=prompt("ingrese sexo");
+        num= parseInt(prompt("ingrese nota"));
+        nota=parseInt(nota)+parseInt(num);
+        if(num<min)
+        {
+            min=num;
+
+        }
+        if(num>=6 && sexo=="m")
+        {
+            contMasc++;
+        }
+        i++;
+    }
+promedio=nota/i;
+alert("El promedio es: "+promedio);
+alert("La nota mas baja es: "+min);
+alert("La cantidad de varones aprobados es: "+contMasc);
+
+
 
 }
